75ce243ecf8c08043fea61b490d4edffb9d116e8
PaulaSoler000 <93978384+PaulaSoler000@users.noreply.github.com>
2024-10-16 09:32:55 +0200

    Update edit.blade.php

--- laravel2/laravel2/resources/views/products/edit.blade.php
+++ laravel2/laravel2/resources/views/products/edit.blade.php
@@ -23,6 +23,16 @@
 
         <input type="submit" value="update">
     </form>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </body>
 
 </html>
